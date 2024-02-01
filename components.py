8dcd84f70392b058a33519ef904392034a76b22a
Julia Piascik <98356859+liapia99@users.noreply.github.com>
2024-02-01 12:13:43 -0500

    Update components.py

--- components.py
+++ components.py
@@ -9,8 +9,8 @@ src_gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
 src_gray = cv.blur(src_gray, (3, 3))
 
 def draw_red_grid(img):
-    # Draw red grid lines with units of 1 cm
-    line_color = (150, 0, 0)  # Red color
+
+    line_color = (150, 0, 0)
     line_thickness = 1
 
     # Define the spacing between grid lines (in pixels) corresponding to 1 cm
@@ -49,13 +49,14 @@ def thresh_callback(val):
             # Filter by elongation = essentially get rid of the wire paths - I mainly use this to get rid of the horizontal and diagonal lines
             elongation_threshold = 1.53  # 1.54 is too high but with 1.53 - two square cannot be identified but that may just be created a bigger connector or making the boxes thicker
             if 0.1 < aspect_ratio < elongation_threshold:
-                cv.drawContours(drawing, [approx], 0, (255, 0, 0), 2)
+                cv.drawContours(drawing, [approx], 0, (255, 255, 255), 2)
 
     # Draw red grid
     draw_red_grid(drawing)
 
     # Show the result
     cv.imshow('Polygons with Grid', drawing)
+    cv.imwrite('processed.jpg', drawing)
 
 # Create Window
 source_window = 'Source'
@@ -66,6 +67,7 @@ max_thresh = 255
 thresh = 100  # Lower initial threshold
 
 thresh_callback(thresh)
+
 
 cv.waitKey(0)
 cv.destroyAllWindows()
